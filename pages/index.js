1a1b8494acfe64f028063a8f8a6abd288077d6aa
Pedro Duran <106774551+Pedro-Duran@users.noreply.github.com>
2023-06-18 22:14:51 +0000

    me passei no ultimo commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1> Sofia, você é o amor de minha vida. 
-        Te amo te amo te amo
+    return <h1> Sofia, você é o amor de minha vida. Minha princesa!! te amo te amo te amo
          </h1>
 }
 
