33792788c072ab648101073ce9cfd9df70d2e9c7
Pedro Duran <106774551+Pedro-Duran@users.noreply.github.com>
2023-06-18 21:54:53 +0000

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1> Sofia, você é o amor de minha vida </h1>
+    return <h1> Sofia, você é o amor de minha vida. 
+        Te amo te amo te amo
+         </h1>
 }
 
 export default Home;
