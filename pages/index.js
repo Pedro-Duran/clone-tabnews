c520927cfc05fc7baa76039246370e9daf7eba4e
Pedro Duran <106774551+Pedro-Duran@users.noreply.github.com>
2023-06-27 08:40:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    return <h1> Sofia, você é o amor de minha vida. Minha princesa!! te amo te amo te amo
-         </h1>
+  return (
+    <h1>
+      {" "}
+      Sofia, você é o amor de minha vida. Minha princesa!! te amo te amo te amo
+    </h1>
+  );
 }
 
 export default Home;
